ddec09adf960e3acc55898810ce48c6b7e997716
Brayan <brayancruz160217@gmail.com>
2022-02-12 11:06:52 -0500

    v2

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
+var codigoDescuento = 'DESCUENTO';
+var descuento = 30; 
+
+function suma (a, b){
+    return a + b
+}
+
+function calculo (a){
+    return a ;
+}
+
 let miNombre = prompt('Ingresa tu nombre');
 let nombreProducto1 = prompt('Escribe el nombre del producto #1');
 let nombreProducto2 = prompt('Escribe el nombre del producto #2');
+let totalDescuento = 0;
 
 /* SE DEFINE EL VALOR DEL PRODUCTO #1 */
 
@@ -18,9 +30,23 @@ while (isNaN(valorProducto2)){
     valorProducto2 = parseInt(prompt('Ingrese el valor del producto #2'))
 }
 
-/* DOCUMENT WRITE */
+suma = valorProducto1 + valorProducto2;
+calculo = formula = descuento * suma;
+calculo = calculo / 100;
+totalDescuento = suma - calculo;
 
-document.write('Hola ', miNombre, ' esta es tu factura <br>')
-document.write(nombreProducto1, ' ----- $', valorProducto1, '<br>')
-document.write(nombreProducto2, ' ----- $', valorProducto2, '<br>')
-document.write('El valor total que debes pagar es $', valorProducto1 + valorProducto2, '<br>')
+
+/* SWITCH */
+
+let aplicarDescuento = prompt('¿Quieres aplicar el descuento? Escribe en mayuscula (SI), si no quieres aplicarlo, escribe "NO"')
+
+switch (aplicarDescuento){
+    case 'SI':
+        document.write('Hola ', miNombre, ' esta es tu factura <br>', nombreProducto1, ' ----- $', valorProducto1, '<br>', nombreProducto2, ' ----- $', valorProducto2, '<br>', 'Aplicaste descuento del ', descuento, '% <br>','El valor total que debes pagar es $', totalDescuento, '<br>')
+    break;
+
+    case 'NO':
+        document.write('Hola ', miNombre, ' esta es tu factura <br>', nombreProducto1, ' ----- $', valorProducto1, '<br>', nombreProducto2, ' ----- $', valorProducto2, '<br>', 'NO APLICASTE EL DESCUENTO <br>', 'El valor total que debes pagar es $', suma, '<br>')    
+    break;
+
+    }
